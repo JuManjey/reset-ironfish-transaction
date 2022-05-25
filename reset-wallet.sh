e892dbd776634ce4efc9c54683d9ef663fa0c427
JuManji <opghld@gmail.com>
2022-05-25 15:47:55 +0300

    Update reset-wallet.sh

--- reset-wallet.sh
+++ reset-wallet.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 systemctl stop ironfishd
+wait (5)
 yarn --cwd ~/ironfish/ironfish-cli/ start accounts:rescan --reset
 #yarn --cwd ~/ironfish/ironfish-cli/ start chain:repair --force --confirm
 yarn --cwd ~/ironfish/ironfish-cli/ start chain:repair --confirm
