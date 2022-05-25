05d56dc26f09ba415885636b3860532a898cc8e1
JuManji <opghld@gmail.com>
2022-05-25 15:45:33 +0300

    Update reset-wallet.sh

--- reset-wallet.sh
+++ reset-wallet.sh
@@ -5,5 +5,4 @@ yarn --cwd ~/ironfish/ironfish-cli/ start accounts:rescan --reset
 yarn --cwd ~/ironfish/ironfish-cli/ start chain:repair --confirm
 systemctl restart ironfishd ironfishd-miner
 systemctl restart cron
-systemctl status ironfishd ironfishd-miner cron
 echo "$(tput setaf 2) Reset is successful"
